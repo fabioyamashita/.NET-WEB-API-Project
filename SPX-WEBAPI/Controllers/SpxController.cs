db0b49a60856482e53bd95cece47808dc11e2da0
Fabio Yamashita <yamashitafabio@gmail.com>
2022-09-13 11:20:51 -0300

    First commit, updating gitignore and adding project instructions file

---
+++ SPX-WEBAPI/Controllers/SpxController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SPX_WEBAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpxController : ControllerBase
+    {
+
+    }
+}
