f2e43c66c918d1b6a932e4a804298eed25c43776
Fabio Yamashita <yamashitafabio@gmail.com>
2022-09-13 15:36:36 -0300

    Adding GET method - Get by Id

--- src/Controllers/SpxController.cs
+++ src/Controllers/SpxController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using SPX_WEBAPI.Domain.Models;
+using SPX_WEBAPI.Infra.Repository;
+using System.Threading.Tasks;
 
 namespace SPX_WEBAPI.Controllers
 {
@@ -6,6 +9,24 @@ namespace SPX_WEBAPI.Controllers
     [Route("[controller]")]
     public class SpxController : ControllerBase
     {
+        private readonly IBaseRepository<Spx> _repository;
 
+        public SpxController(IBaseRepository<Spx> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            var spxRecord = await _repository.GetByKey(id);
+
+            if (spxRecord == null)
+            {
+                return NotFound("Id not found");
+            }
+
+            return Ok(spxRecord);
+        }
     }
 }
