26140853a00e11e85507bcb1b29b1679f580750f
Fabio Yamashita <yamashitafabio@gmail.com>
2022-10-08 09:37:00 -0300

    feat: Add Login Controller Integration tests

--- test/SPX-WEBAPI.Tests/IntegrationTests/LoginControllerIntegrationTests.cs
+++ test/SPX-WEBAPI.Tests/IntegrationTests/LoginControllerIntegrationTests.cs
@@ -1,5 +1,7 @@
-﻿using SPX_WEBAPI.AuthorizationAndAuthentication;
+﻿using FluentAssertions;
+using SPX_WEBAPI.AuthorizationAndAuthentication;
 using SPX_WEBAPI.Tests.IntegrationTests.Config;
+using System.Net;
 using System.Net.Http.Json;
 using Xunit;
 
@@ -15,7 +17,7 @@ namespace SPX_WEBAPI.Tests.IntegrationTests
             _testsFixture = testsFixture;
         }
 
-        [Fact(DisplayName = "Authenticate ValidLogin Returns Status Code 200")]
+        [Fact(DisplayName = "Authenticate Valid Login Returns Status Code 200")]
         [Trait("SPX", "Login Controller Integration Tests")]
         public async Task Authenticate_ValidLogin_ReturnsStatusCode200()
         {
@@ -31,7 +33,46 @@ namespace SPX_WEBAPI.Tests.IntegrationTests
 
             // Assert
             getResponse.EnsureSuccessStatusCode();
+            getResponse.StatusCode.Should().Be(HttpStatusCode.OK);
 
+        }
+
+        [Fact(DisplayName = "Authenticate Invalid Login And Correct Password Returns Status Code 401")]
+        [Trait("SPX", "Login Controller Integration Tests")]
+        public async Task Authenticate_InvalidLoginAndCorrectPassword_ReturnsStatusCode401()
+        {
+            // Arrange
+            var userData = new Authenticate
+            {
+                Login = "wrongUser",
+                Password = "m1nh@s3nh@"
+            };
+
+            // Act
+            var getResponse = await _testsFixture.Client.PostAsJsonAsync("/Login", userData);
+
+            // Assert
+            getResponse.IsSuccessStatusCode.Should().BeFalse();
+            getResponse.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact(DisplayName = "Authenticate Correct Login And invalid Password Returns Status Code 401")]
+        [Trait("SPX", "Login Controller Integration Tests")]
+        public async Task Authenticate_CorrectLoginAndInvalidPassword_ReturnsStatusCode401()
+        {
+            // Arrange
+            var userData = new Authenticate
+            {
+                Login = "usuario",
+                Password = "wrongPassword"
+            };
+
+            // Act
+            var getResponse = await _testsFixture.Client.PostAsJsonAsync("/Login", userData);
+
+            // Assert
+            getResponse.IsSuccessStatusCode.Should().BeFalse();
+            getResponse.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
         }
     }
 }
